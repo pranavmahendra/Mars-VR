e5b663f389493a38e353c4c45c1e2c82a7913439
Pranav Mahendra <>
2020-06-14 11:49:09 +0530

    Some files update to 2019 lts. Scrptis added.

---
+++ Assets/Scripts/MaterialGradient.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialGradient : MonoBehaviour
+{
+
+    private Renderer textRenderer;
+
+    [SerializeField]
+    Gradient gradient;
+    private float gradientPos; //GradientPos will chnage as per the angle values derived from camera
+    void setGradientPos(float position)
+    {
+        if(position == gradientPos)
+        {
+            return;
+        }
+        else
+        {
+            gradientPos = position;
+            textRenderer.material.color = gradient.Evaluate(gradientPos);
+        }
+    }
+
+    void Awake()
+    {
+         textRenderer = gameObject.GetComponent<Renderer>();
+    }
+
+  
+    void Start()
+    {
+        //Starting gradient pos value.
+        gradientPos = 0;
+    }
+
+   
+    void Update()
+    {
+         setGradientPos(Mathf.Sin(((Time.time) * 0.5f) + 0.5f));
+    }
+}
---
+++ Assets/Scripts/ObjectFocus.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFocus : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform reference;
+    public float fadeAmount;
+    public float delta;
+
+    void Awake()
+    {
+        reference = Camera.main.gameObject.transform;
+    }
+    
+    void Start()
+    {
+        
+        Debug.DrawLine(reference.transform.position, transform.position,Color.magenta);
+
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
